2f36f614b3b5cd8c08006e36ad292b619781d00e
Shrithik Shahapure <shrithik.shahapure12@gmail.com>
2023-07-27 00:12:23 +0530

    Update extract_chapters.py

--- extract_chapters.py
+++ extract_chapters.py
@@ -2,7 +2,7 @@ from extract_transcript import  extract_transcript
 from palm_api_chapters import  palm_api_chapters
 import streamlit as st
 
-#passes video link to extract transcript and generate Youtube Chapters using OpenAI
+#passes video link to extract transcript and generate Youtube Chapters using Google PaLM
 def extract_chapters(video_link):
     video_transcript = extract_transcript(video_link)
     chapters = palm_api_chapters(video_transcript)

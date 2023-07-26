93269f0343cdbb775f8b3cfc908e5a48644c1861
Shrithik Shahapure <shrithik.shahapure12@gmail.com>
2023-07-27 00:11:51 +0530

    Update extract_summary.py

--- extract_summary.py
+++ extract_summary.py
@@ -1,13 +1,11 @@
 from extract_transcript import  extract_transcript
-from open_ai_summary import open_ai_summary
+from palm_api_summary import palm_api_summary
 import streamlit as st
-import pyperclip
 
-#passes video link to extract transcript and generate Youtube Chapters using OpenAI
+#passes video link to extract transcript and generate Youtube Chapters using Google PaLM
 
 def extract_summary(video_link):
     video_transcript = extract_transcript(video_link)
-    summary = open_ai_summary(video_transcript)
-    st.success('Done! Added to clipboard.')
-    pyperclip.copy(summary)         # use pyperclip to add the transcript to the clipboard automatically
-    st.text(summary)
+    summary = palm_api_summary(video_transcript)
+    st.success('Done!')
+    st.write(summary)

9026b6437ef7f77041cfa9758102bf89315fabca
Shrithik Shahapure <shrithik.shahapure12@gmail.com>
2023-07-27 00:10:55 +0530

    Update extract_chapters.py

--- extract_chapters.py
+++ extract_chapters.py
@@ -1,12 +1,10 @@
 from extract_transcript import  extract_transcript
-from open_ai_chapters import  open_ai_chapters
+from palm_api_chapters import  palm_api_chapters
 import streamlit as st
-import  pyperclip
 
 #passes video link to extract transcript and generate Youtube Chapters using OpenAI
 def extract_chapters(video_link):
     video_transcript = extract_transcript(video_link)
-    chapters = open_ai_chapters(video_transcript)
-    st.success('Done! Added to clipboard.')
-    pyperclip.copy(chapters)            # use pyperclip to add the chapters to the clipboard automatically
-    st.text(chapters)
+    chapters = palm_api_chapters(video_transcript)
+    st.success('Done!')
+    st.write(chapters)
